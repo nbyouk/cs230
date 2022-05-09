7ebf138134501e27d82d8b9d8747a5b95ac16a7d
Noah Youkilis <nbyouk@gmail.com>
2022-05-09 11:56:44 -0700

    finished the data conversion script

--- data_tools.m
+++ data_tools.m
@@ -1,32 +1,94 @@
 mat2csv()
 
 function mat2csv()
-% data = load(append('matlab_data/', mat_name, '.mat')).CaseData;
-[ecg, ann] = load_patient(1);
-% ecg = timetable2table(data.ECG, "ConvertRowTimes", false);
+rng(63)
+assignments = [randperm(63, 63)]';
 
-% writematrix(ecg{:, :}, append(mat_name, '_data.csv'))
-% writematrix(anntype, append(mat_name, '_labels.csv'))
+% train data
+labels_full = zeros(53*60, 1);
+sample_nums = [randperm(53*60, 53*60)]';
+for i = 1:53
+    [ecg, labels] = load_patient(assignments(i));
+    mult = 30000;
+    for k = 1:60
+        ecg_minute = ecg((k-1)*mult+1:k*mult, :);
+        writematrix(ecg_minute, append('aws_bucket/train/sample', string(sample_nums(60*(i-1)+k)-1), '.csv'))
+        labels_full(sample_nums(60*(i-1)+k)) = labels(k);
+    end
+    writematrix(labels_full, 'aws_bucket/train/labels.csv')
 end
 
-function [ecg, anntype] = load_patient(num)
+% dev data
+labels_full = zeros(5*60, 1);
+sample_nums = [randperm(5*60, 5*60)]';
+for i = 1:5
+    [ecg, labels] = load_patient(assignments(i+53));
+    mult = 30000;
+    for k = 1:60
+        ecg_minute = ecg((k-1)*mult+1:k*mult, :);
+        writematrix(ecg_minute, append('aws_bucket/dev/sample', string(sample_nums(60*(i-1)+k)-1), '.csv'))
+        labels_full(sample_nums(60*(i-1)+k)) = labels(k);
+    end
+    writematrix(labels_full, 'aws_bucket/dev/labels.csv')
+end
+
+% test data
+labels_full = zeros(5*60, 1);
+sample_nums = [randperm(5*60, 5*60)]';
+for i = 1:5
+    [ecg, labels] = load_patient(assignments(i+58));
+    mult = 30000;
+    for k = 1:60
+        ecg_minute = ecg((k-1)*mult+1:k*mult, :);
+        writematrix(ecg_minute, append('aws_bucket/test/sample', string(sample_nums(60*(i-1)+k)-1), '.csv'))
+        labels_full(sample_nums(60*(i-1)+k)) = labels(k);
+    end
+    writematrix(labels_full, 'aws_bucket/test/labels.csv')
+end
+end
+
+function [ecg, labels] = load_patient(num)
 data = load(append('aws_bucket/matlab_data/patient', string(num), '.mat')).CaseData;
-ecg = timetable2table(data.ECG, "ConvertRowTimes", false){:,:};
-anntype = data.anntype;
-ann = convert_ann;
+ecg = timetable2table(data.ECG, "ConvertRowTimes", false);
+ecg = ecg{:,:};
+labels = convert_ann(data);
 end
 
-function ann = convert_ann(anntype)
+function labels = convert_ann(data)
+ann = data.ann;
+anntype = data.anntype;
 afib = strcmp(anntype, '(AFIB');
 normal = strcmp(anntype, '(N'); % add all other anntypes to exclude here
-ann = zeros(size(afib));
-for i = 1:length(ann)
+% populate missing anntype entries
+anntype = zeros(size(anntype));
+for i = 1:length(anntype)
     if afib(i) == 1
-        ann(i) = 1;
+        anntype(i) = 1;
     elseif normal(i) == 1
-        ann(i) = 0;
+        anntype(i) = 0;
     else
-        ann(i) = ann(i - 1);
+        if i-1 == 0
+            anntype(i) = 0;
+        else
+            anntype(i) = anntype(i - 1);
+        end
+    end
+end
+
+% expand to fullsize vector of anntype
+labels_full = zeros(1.8e6, 1);
+for i = 1:length(ann)-1
+    labels_full(ann(i):ann(i+1)) = anntype(i);
+end
+labels_full(ann(end):end) = anntype(end);
+labels_full(1:ann(1)) = anntype(1);
+
+% collapse to minute-chunk labels vector
+labels = zeros(60, 1);
+mult = 30000;
+for i = 1:length(labels)
+    if sum(labels_full((i-1)*mult+1:i*mult)) / mult > 0.5
+        labels(i) = 1;
     end
 end
 end
