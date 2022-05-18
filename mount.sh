4a399e3b9cf1b6dd878c97cf42bcf3da73d76d0f
Noah Youkilis <nbyouk@gmail.com>
2022-05-18 13:54:14 -0700

    trying more mount fixes

--- mount.sh
+++ mount.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-s3fs cs230-af-data aws_bucket -o passwd_file=${HOME}/.passwd-s3fs
-
+s3fs cs230-af-data aws_bucket -o passwd_file=${HOME}/.passwd-s3fs -o url=https://s3.us-west-1.amazonaws.com
