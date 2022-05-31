43c9c162b8e2b51f4242a36e971a80c6617cf83f
Noah Youkilis <nbyouk@gmail.com>
2022-05-31 15:33:46 -0700

    final touches to plotting for RNN

--- rnn/plot_metrics.py
+++ rnn/plot_metrics.py
@@ -12,19 +12,15 @@ arg = parser.parse_args()
 
 df_train = pd.read_csv(arg.trainfile)
 df_val = pd.read_csv(arg.valfile)
-num_batches = len(df_train)
-bpe_train = 318
-bpe_val = 38
-x_train = np.linspace(0, len(df_train) // bpe_train * 10, len(df_train))
-x_val = np.linspace(0, len(df_val) // bpe_val, len(df_val))
+x = np.linspace(1, 20, 20)
 
 fig, axs = plt.subplots(2, 1, figsize=(20,16))
-loss_train = df_train["loss"].rolling(20, center=True).mean().values
-accuracy_train = df_train["accuracy"].rolling(20, center=True).mean().values
-accuracy_val = df_val["accuracy"].rolling(38).mean().values
-axs[0].plot(x_train, loss_train, label='Train', linewidth=4)
-axs[1].plot(x_train, accuracy_train, label='Train', linewidth=4)
-axs[1].plot(x_val, accuracy_val, label='Test', linewidth=4)
+loss_train = df_train["loss"]
+accuracy_train = df_train["accuracy"]
+accuracy_val = df_val["accuracy"]
+axs[0].plot(x, loss_train, label='Train', linewidth=4)
+axs[1].plot(x, accuracy_train, label='Train', linewidth=4)
+axs[1].plot(x, accuracy_val, label='Test', linewidth=4)
 
 
 axs[0].set_title('Train Loss', fontsize=30)
